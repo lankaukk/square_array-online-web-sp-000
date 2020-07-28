a1a87f124986f39a51b1e8d15204c96cca095d5d
McKayla Lankau <lankaukk@yahoo.com>
2020-07-28 19:42:50 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,7 +3,7 @@
 def square_array(numbers)
   squared_numbers =[]
   numbers.each do |number|
-    squared_numbers << number**number 
+    squared_numbers << number*number 
   end
   squared_numbers
 end
